cdb1705652b0dd4ee5efd460a5f0a76dee0ce163
aiandstuffs-netizen <aiandstuffs@gmail.com>
2025-11-29 10:23:13 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,17 @@
 
 import React, { useContext, useState, useEffect } from "react";
 import Image from "next/image";
+import Link from "next/link"; // For clean social redirects
 
 import { ICOContext } from "../context/ERC20ICO";
 import Style from "../styles/index.module.css";
 import zKrypt from "../assets/zKrypt.jpg";
+
+// Social media icons (add these to your public folder or use CDN)
+import GitHubIcon from "../assets/github-icon.png"; // 32x32 PNG
+import DiscordIcon from "../assets/discord-icon.png";
+import TelegramIcon from "../assets/telegram-icon.png";
+import XIcon from "../assets/x-twitter-icon.png";
 
 import NavBar from "../components/NavBar/NavBar";
 import User from "../components/User/User";
@@ -83,7 +90,72 @@ const Home = () => {
               <button className={Style.btn}>Whitepaper</button>
               <button className={Style.btn}>Product Intro</button>
             </div>
+
+            {/* NEW: Social Media Icons Section */}
+            <div className={Style.socialIconsContainer}>
+              <h3 style={{ margin: "20px 0 10px 0", fontSize: "18px", color: "#333" }}>
+                Join the Community
+              </h3>
+              <div className={Style.socialIcons}>
+                {/* GitHub Icon */}
+                <Link href="https://github.com/kalabmesfin/zKrypt-FHE-ICO" target="_blank" rel="noopener noreferrer">
+                  <div className={Style.socialIconWrapper}>
+                    <Image 
+                      src={GitHubIcon} 
+                      alt="GitHub" 
+                      width={32} 
+                      height={32}
+                      className={`${Style.socialIcon} ${Style.githubIcon}`}
+                    />
+                    <span className={Style.iconTooltip}>GitHub</span>
+                  </div>
+                </Link>
+
+                {/* Discord Icon */}
+                <Link href="https://discord.gg/phantomexile" target="_blank" rel="noopener noreferrer">
+                  <div className={Style.socialIconWrapper}>
+                    <Image 
+                      src={DiscordIcon} 
+                      alt="Discord" 
+                      width={32} 
+                      height={32}
+                      className={`${Style.socialIcon} ${Style.discordIcon}`}
+                    />
+                    <span className={Style.iconTooltip}>Discord</span>
+                  </div>
+                </Link>
+
+                {/* Telegram Icon */}
+                <Link href="https://t.me/codeforchrist" target="_blank" rel="noopener noreferrer">
+                  <div className={Style.socialIconWrapper}>
+                    <Image 
+                      src={TelegramIcon} 
+                      alt="Telegram" 
+                      width={32} 
+                      height={32}
+                      className={`${Style.socialIcon} ${Style.telegramIcon}`}
+                    />
+                    <span className={Style.iconTooltip}>Telegram</span>
+                  </div>
+                </Link>
+
+                {/* X (Twitter) Icon */}
+                <Link href="https://x.com/zialch" target="_blank" rel="noopener noreferrer">
+                  <div className={Style.socialIconWrapper}>
+                    <Image 
+                      src={XIcon} 
+                      alt="X (Twitter)" 
+                      width={32} 
+                      height={32}
+                      className={`${Style.socialIcon} ${Style.xIcon}`}
+                    />
+                    <span className={Style.iconTooltip}>X</span>
+                  </div>
+                </Link>
+              </div>
+            </div>
           </div>
+
           <div className={Style.heroSection_right}>
             <Image src={zKrypt} alt="banner" width={300} height={300} className={Style.heroSection_right_img_one} />
             <Image src={zKrypt} alt="banner" width={200} height={200} className={Style.heroSection_right_img} />
