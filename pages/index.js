d1330d32443ff2938854520df6cbafe12b21ddc7
aiandstuffs-netizen <aiandstuffs@gmail.com>
2025-11-29 10:46:58 +0300

    Added social icons

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,17 @@
 
 import React, { useContext, useState, useEffect } from "react";
 import Image from "next/image";
-import Link from "next/link"; // For clean social redirects
+import Link from "next/link";
 
 import { ICOContext } from "../context/ERC20ICO";
 import Style from "../styles/index.module.css";
 import zKrypt from "../assets/zKrypt.jpg";
 
-// Social media icons (add these to your public folder or use CDN)
-import GitHubIcon from "../assets/github-icon.png"; // 32x32 PNG
+// Social media icons (place in public/assets/icons/)
+import GitHubIcon from "../assets/github-icon.png";
 import DiscordIcon from "../assets/discord-icon.png";
 import TelegramIcon from "../assets/telegram-icon.png";
-import XIcon from "../assets/x-twitter-icon.png";
+import XIcon from "../assets/x-icon.png";
 
 import NavBar from "../components/NavBar/NavBar";
 import User from "../components/User/User";
@@ -91,68 +91,31 @@ const Home = () => {
               <button className={Style.btn}>Product Intro</button>
             </div>
 
-            {/* NEW: Social Media Icons Section */}
-            <div className={Style.socialIconsContainer}>
-              <h3 style={{ margin: "20px 0 10px 0", fontSize: "18px", color: "#333" }}>
-                Join the Community
-              </h3>
-              <div className={Style.socialIcons}>
-                {/* GitHub Icon */}
-                <Link href="https://github.com/kalabmesfin/zKrypt-FHE-ICO" target="_blank" rel="noopener noreferrer">
-                  <div className={Style.socialIconWrapper}>
-                    <Image 
-                      src={GitHubIcon} 
-                      alt="GitHub" 
-                      width={32} 
-                      height={32}
-                      className={`${Style.socialIcon} ${Style.githubIcon}`}
-                    />
-                    <span className={Style.iconTooltip}>GitHub</span>
-                  </div>
-                </Link>
+            {/* PROFESSIONAL SIDE-BY-SIDE SOCIAL ICONS - NO WRAPPING */}
+            <div className={Style.socialIconsPro}>
+              <Link href="https://github.com/kalabmesfin/zKrypt-FHE-ICO" target="_blank" rel="noopener noreferrer" className={Style.socialLink}>
+                <div className={`${Style.socialBtn} ${Style.github}`}>
+                  <Image src={GitHubIcon} alt="GitHub" width={24} height={24} />
+                </div>
+              </Link>
 
-                {/* Discord Icon */}
-                <Link href="https://discord.gg/phantomexile" target="_blank" rel="noopener noreferrer">
-                  <div className={Style.socialIconWrapper}>
-                    <Image 
-                      src={DiscordIcon} 
-                      alt="Discord" 
-                      width={32} 
-                      height={32}
-                      className={`${Style.socialIcon} ${Style.discordIcon}`}
-                    />
-                    <span className={Style.iconTooltip}>Discord</span>
-                  </div>
-                </Link>
+              <Link href="https://discord.gg/phantomexile" target="_blank" rel="noopener noreferrer" className={Style.socialLink}>
+                <div className={`${Style.socialBtn} ${Style.discord}`}>
+                  <Image src={DiscordIcon} alt="Discord" width={24} height={24} />
+                </div>
+              </Link>
 
-                {/* Telegram Icon */}
-                <Link href="https://t.me/codeforchrist" target="_blank" rel="noopener noreferrer">
-                  <div className={Style.socialIconWrapper}>
-                    <Image 
-                      src={TelegramIcon} 
-                      alt="Telegram" 
-                      width={32} 
-                      height={32}
-                      className={`${Style.socialIcon} ${Style.telegramIcon}`}
-                    />
-                    <span className={Style.iconTooltip}>Telegram</span>
-                  </div>
-                </Link>
+              <Link href="https://t.me/codeforchrist" target="_blank" rel="noopener noreferrer" className={Style.socialLink}>
+                <div className={`${Style.socialBtn} ${Style.telegram}`}>
+                  <Image src={TelegramIcon} alt="Telegram" width={24} height={24} />
+                </div>
+              </Link>
 
-                {/* X (Twitter) Icon */}
-                <Link href="https://x.com/zialch" target="_blank" rel="noopener noreferrer">
-                  <div className={Style.socialIconWrapper}>
-                    <Image 
-                      src={XIcon} 
-                      alt="X (Twitter)" 
-                      width={32} 
-                      height={32}
-                      className={`${Style.socialIcon} ${Style.xIcon}`}
-                    />
-                    <span className={Style.iconTooltip}>X</span>
-                  </div>
-                </Link>
-              </div>
+              <Link href="https://x.com/zialch" target="_blank" rel="noopener noreferrer" className={Style.socialLink}>
+                <div className={`${Style.socialBtn} ${Style.x}`}>
+                  <Image src={XIcon} alt="X" width={24} height={24} />
+                </div>
+              </Link>
             </div>
           </div>
 
